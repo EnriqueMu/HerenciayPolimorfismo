3f8229efd20ce9fc0ac22c081525535a8892b65c
henry <henrykonnor@gmail.com>
2017-03-07 12:02:00 -0600

    nuevo proyecto
    se creo un proyecto nuevo para transformar un arraylist en un arreglo y
    se ordenaron los datos

---
+++ Ejercicio Promedio/Banco.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Banco here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Banco
+{
+  /* //ArrayList <CuentaBancaria> col = new ArrayList <CuentaBancaria>();
+     
+     private ArrayList <CuentaBancaria> col;
+     public Banco()
+     {
+         col=new ArrayList <CuentaBancaria>();
+     }
+ 
+     public void agregaCuenta(CuentaBancaria cuenta)
+     {
+         col.add(cuenta);
+     }*/
+     
+}
---
+++ Ejercicio Promedio/Planeta.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Planeta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Planeta
+{
+ /* private ArrayList<Pais> paises;
+     
+     public Planeta()
+     {
+         paises =new ArrayList<Pais>();
+     }
+     
+     public void agragaPlaneta(Pais p)
+     {
+         paises.add(p);
+    }*/
+}
