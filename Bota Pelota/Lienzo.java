477dcff04ed2b21e0f75fa9e880162ec9b431eb2
henry <henrykonnor@gmail.com>
2017-03-13 12:02:48 -0600

    escuchadro del raton
    se agrego el escuchador del raton para que aparecieran nuevas pelotas en
    el lugar donde se hace click

--- Bota Pelota/Lienzo.java
+++ Bota Pelota/Lienzo.java
@@ -1,5 +1,9 @@
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+import java.util.LinkedList;
+import java.awt.Rectangle;
 
 /**
  * Write a description of class Lienzo here.
@@ -9,24 +13,46 @@ import java.awt.Graphics;
  */
 public class Lienzo extends JPanel 
 {
-   private  Pelota pelota;
+   private  LinkedList<Pelota> pelotas;
    
    public Lienzo()
    {
-       pelota = new Pelota(100,100,10);
-      
-      
+       pelotas = new LinkedList<Pelota>();
+       pelotas.add(new Pelota(100,100,10));
+       EscuchadorRaton escuchaRaton = new EscuchadorRaton();
+       this.addMouseListener(escuchaRaton);
     } 
     public void actua()
     {
-        pelota.muevete(this.getBounds());
+        Rectangle r = this.getBounds();
+        //le indica a cada pelota que se mueva
+        for(Pelota p : pelotas){
+            p.muevete(this.getBounds());
+        }
         this.repaint();
+    }
+    class EscuchadorRaton extends MouseAdapter
+    {
+        @Override
+        public void mouseClicked(MouseEvent me)
+        {
+            //genera un numero aleatorio para el tamaño
+            int tamRandom = 10 + (int)(Math.random()*20);
+            Pelota pelotaNueva = new Pelota(me.getX(), me.getY(),tamRandom);
+            pelotas.add(pelotaNueva);
+        }
+         
     }
     
     @Override
     public void paintComponent(Graphics g)
     {
-        pelota.dibujate(g);
+        //borra el panel para dubujar las pelotas en una nueva posicion
+        super.paintComponent(g);
+        //dibuja las pelotas
+        for(Pelota p : pelotas){
+            p.dibujate(g);
+        }
     }
 }
 
